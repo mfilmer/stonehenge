754b3c86a66205039c739f60291f37e53da827ed
Matthew Filmer <mfilmer@nd.edu>
2017-05-03 23:53:25 -0400

    Started working on csv import

--- BuiltinFunctions.hs
+++ BuiltinFunctions.hs
@@ -1,6 +1,13 @@
 module BuiltinFunctions where
 
+import qualified Data.Csv.Parser as CSV
+
 import Workspace
 
 biMax :: [SHData] -> SHData
 biMax ((DataValue a):(DataValue b):_) = DataValue $ max a b
+
+loadMatrix :: String -> IO (SHMatrix)
+loadMatrix file = do
+  contents <- readFile file
+  CSV.csv CSV.defaultDecodeOptions
